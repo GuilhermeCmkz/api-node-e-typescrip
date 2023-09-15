03b9eecda96072d7f658d9a6aa249c34306c7d83
GuilhermeCmkz <guilherme_costa@outlook.com>
2023-09-15 09:35:52 -0300

    feat: ultimas alterações

---
+++ src/server/controllers/cidades/Create.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+
+
+
+
+export const create = (req: Request, res: Response) => {
+
+
+  return res.send('Create!');
+};
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,16 +1,15 @@
 import { Router } from 'express';
 import { StatusCodes} from 'http-status-codes';
 
+import {CidadesController} from './../controllers';
+
+
 const router = Router();
 
 router.get('/', (_, res) => {
   return res.send('Olá, DEV!');
 });
 
-router.post('/teste', (req, res) => {
-// console.log(req.query.teste);
-
-  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post('/cidades', CidadesController.create);
 
 export { router };
