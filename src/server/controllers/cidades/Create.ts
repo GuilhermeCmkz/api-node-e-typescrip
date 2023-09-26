cb9532011b1e81e4c7c916cc5624b90bf928b679
GuilhermeCmkz <guilherme_costa@outlook.com>
2023-09-26 15:38:42 -0300

    feat: aula 09 Validação

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,9 +1,35 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
 
 
+interface ICidade{
+  nome: string;
+}
+
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+  nome: yup.string().required().min(3),
+});
 
 
-export const create = (req: Request, res: Response) => {
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined = undefined;
+
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error){
+    const yupError = error as yup.ValidationError;
+
+    return res.json({
+      errors: {
+        default: yupError.message,
+      }
+
+    });
+
+  }
+  
+  console.log(req.body);
 
 
   return res.send('Create!');
